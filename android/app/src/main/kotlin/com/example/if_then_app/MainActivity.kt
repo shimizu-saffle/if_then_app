7159c875af49a8a6ba83588e359608521146ffb8
shimizu-saffle <shimizu.saffle@gmail.com>
2021-07-12 19:58:04 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/if_then_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.if_then_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'If-Then plans',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'If-Then plans'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'If → Then',
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(),
+    );
+  }
+}
+
+// items: [
+// BottomNavigationBarItem(icon: Icon(Icons.create)),
+// BottomNavigationBarItem(icon: Icon(Icons.create)),
+// BottomNavigationBarItem(icon: Icon(Icons.create)),
+// ],

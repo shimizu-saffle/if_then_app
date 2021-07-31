18c9e71fdc9268b9a9104c41184a18a7aef389e3
shimizu-saffle <shimizu.saffle@gmail.com>
2021-07-31 19:55:07 +0900

    extract widgetからTextInputFieldクラスを作成

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'If-Then',
-      home: TextInputField(),
+      home: EntrancePage(),
       // home: HomePage(),
     );
   }
--- lib/views/entrance_page.dart
+++ lib/views/entrance_page.dart
@@ -18,38 +18,7 @@ class EntrancePage extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: Container(
-              height: 70,
-              decoration: BoxDecoration(
-                color: Colors.blue.withOpacity(0.7),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0,
-                      ),
-                      child: Icon(
-                        FontAwesomeIcons.envelope,
-                        size: 28,
-                        color: Colors.white,
-                      ),
-                    ),
-                    hintText: 'メールアドレス',
-                    hintStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.next,
-                ),
-              ),
-            ),
-          ),
+          TextInputField(),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10.0),
             child: Container(
@@ -92,90 +61,51 @@ class EntrancePage extends StatelessWidget {
 class TextInputField extends StatelessWidget {
   const TextInputField({
     Key? key,
+    this.icon,
+    this.hint,
+    this.inputType,
+    this.inputAction,
   }) : super(key: key);
+
+  final IconData? icon;
+  final String? hint;
+  final TextInputType? inputType;
+  final TextInputAction? inputAction;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Flexible(
-            child: Center(
-              child: Text(
-                'If-Then',
-                style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 60,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: Container(
-              height: 70,
-              decoration: BoxDecoration(
-                color: Colors.blue.withOpacity(0.7),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0,
-                      ),
-                      child: Icon(
-                        FontAwesomeIcons.envelope,
-                        size: 28,
-                        color: Colors.white,
-                      ),
-                    ),
-                    hintText: 'メールアドレス',
-                    hintStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.next,
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Container(
+        height: size.height * 0.08,
+        width: size.width * 0.8,
+        decoration: BoxDecoration(
+          color: Colors.blue.withOpacity(0.7),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Center(
+          child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                ),
+                child: Icon(
+                  FontAwesomeIcons.envelope,
+                  size: 28,
+                  color: Colors.white,
                 ),
               ),
+              hintText: hint,
+              hintStyle: TextStyle(color: Colors.white),
             ),
+            style: TextStyle(color: Colors.white),
+            keyboardType: inputType,
+            textInputAction: TextInputAction.next,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: Container(
-              height: 70,
-              decoration: BoxDecoration(
-                color: Colors.blue.withOpacity(0.7),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0,
-                      ),
-                      child: Icon(
-                        FontAwesomeIcons.lock,
-                        size: 28,
-                        color: Colors.white,
-                      ),
-                    ),
-                    hintText: 'パスワード',
-                    hintStyle: TextStyle(color: Colors.white),
-                  ),
-                  obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                  keyboardType: TextInputType.name,
-                  textInputAction: TextInputAction.done,
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }

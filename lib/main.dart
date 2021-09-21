f4dce2ac3bb237b11d82c44101f2223644d37a6a
shimizu-saffle <shimizu.saffle@gmail.com>
2021-09-21 16:19:10 +0900

    開発環境用のFirebassプロジェクトの紐付けAndroid

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'イフゼン',
       initialRoute: '/',
       routes: {
